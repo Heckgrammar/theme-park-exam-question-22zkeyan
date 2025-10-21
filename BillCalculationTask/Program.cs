3b7002fdf212ceffa10d9e5e68cdc78bb8c36d7c
22zkeyan <22zkeyan@heckgrammar.co.uk>
2025-10-21 13:34:10 +0100

    Marked

--- BillCalculationTask/Program.cs
+++ BillCalculationTask/Program.cs
@@ -5,19 +5,19 @@
         static int CalculateCost(int people)
         {
             const int cost_per_person = 15;
-            int total = cost_per_person * people;
-            if (people >= 6)
+            int total = cost_per_person * people; //Mark C - multiplying people by 15
+            if (people >= 6) //Mark B - selection //Mark D - correct boolean expression
             {
-                total -= 5;
+                total -= 5; //Mark E - reduce total by 5
             }
             return total;
         }
         static void Main(string[] args)
         {
             Console.Write("Enter the number of people: ");
-            int people = Convert.ToInt32(Console.ReadLine()!);
+            int people = Convert.ToInt32(Console.ReadLine()!); //Mark A - inputting no. of people
             int total = CalculateCost(people);
-            Console.WriteLine($"The total is £{total}");
+            Console.WriteLine($"The total is £{total}"); //Mark F - outputting final cost
         }
     }
 }
